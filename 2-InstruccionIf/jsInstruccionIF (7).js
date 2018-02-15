a7f94886fe2ba68dc29b0f0aafe5eae5dbc24f69
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-02-15 10:26:40 -0300

    ejercicios 7 y 8 en clase

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
 
+edad = document.getElementById("edad").value;
+estadoCivil = document.getElementById("estadoCivil").value;
+
+//con el !="Soltero" estoy negando eso, entonces cuando no sea soltero va a funcionar.
+
+if(edad < 18 && estadoCivil != "Soltero")
+{
+    alert("Es muy pequeño para NO se soltero")
+}
 	
 
 
