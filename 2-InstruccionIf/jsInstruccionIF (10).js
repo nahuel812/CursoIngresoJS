abaad505a77a476bf16dde70fb838a235b8d6b41
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-02-15 12:16:49 -0300

    ejercicios 9 y 10 en clase
    if, else if, else, generar numero random con Math.random( ) con maximos y minimos y console.log

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,62 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
 
+	nota = parseInt(Math.random()*(10-1)+1);
+
+	console.log(nota);
+	
+	if(nota >= 9)
+	{
+		alert("Excelente")
+	}
+	else if(nota >= 6)
+	{
+		alert("Muy bien")
+	}
+	else if(nota >= 4)
+	{
+		alert("Bien")
+	}
+	else if(nota >= 2)
+	{
+		alert("Mal")
+	}
+	else
+	{
+		alert("Desastroso")
+	}
+/*
+	if(nota >= 9)
+	{
+		alert("Excelente")
+	}
+	else
+	{
+		if(nota >= 6)
+		{
+			alert("Muy bien")
+		}
+		else
+		{
+			if(nota >= 4)
+			{
+				alert("Bien")
+			}
+			else
+			{
+				if(nota >= 2)
+				{
+					alert("Mal")
+				}
+				else
+				{
+					alert("Desastroso")
+				}
+			}
+		}
+	}
+
+	*/
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,11 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numero;
 	
+	//para generar un numero random entre los valores que quiero Math.random()*(max-min)+min
+
+	numero = parseInt(Math.random()*(11-1)+1);
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
