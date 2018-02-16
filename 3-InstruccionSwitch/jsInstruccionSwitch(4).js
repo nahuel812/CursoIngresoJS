dfccecc7279a86d009179f5907a755fc66e0634f
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-02-16 10:22:43 -0300

    ejercicios del 1 al 6 de switch en clase
    switch, case, default y break

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,27 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    //tomo la edad  
+    var mesDelAño;
+    mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+    //alert (mesDelAño);
+    switch(mesDelAño)
+    {
+        case "Febrero":
+            alert("Este mes tiene 28 dias.")
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+        case "Diciembre":
+            alert("Este mes tiene 30 dias")
+            break;
+        default:
+            alert("Este mes tiene 31 dias")
+            break;
+        
+    }
 	
 	
 
