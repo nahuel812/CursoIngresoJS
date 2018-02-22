28d7e1f8ab75da30813ed8ca6821aeef45480d4e
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-02-22 12:15:02 -0300

    ejercicios 5 y 6 en clase
    while y acumulador

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,16 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+var sexo;
+sexo = prompt("ingrese f ó m .");
+
+while((sexo!="f" && sexo!="m") && sexo !=null)
+{
+    alert("Letra incorrecta")
+    sexo = prompt("ingrese f ó m .");
+}
 
 
-
-document.getElementById('Sexo').value=sexo;
+document.getElementById("Sexo").value=sexo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,32 @@
 function Mostrar()
 {
-
+	var numero;
 	var contador=0;
 	var acumulador=0;
 
+	while(contador<5)
+	{
+		contador ++
+		numero = prompt("ingrese otro numero")
+		numero = parseInt(numero)//si ingreso una letra va a valer NaN
+		acumulador=acumulador+numero
+		//o tambien para acumular acumular+=numero;
+		//para acumular a una variable le sumo otra y se acumula.
+		
 
+	}
+	
+	/*
+	while(isNaN(NaN))
+		{
+			numero=prompt()
+			numero=parseInt(numero)
+		}
+	*/
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
+
+	
 
 }//FIN DE LA FUNCIÓN
