207da7fcbe442623654b79b8127e6aae8cd3454b
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-02-22 18:58:50 -0300

    ejercicios 7 y 8 while
    while, while con if,y isNaN

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,9 +1,28 @@
 function Mostrar()
 {
-
+	var numero;
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+
+	respuesta = prompt("Desea ingresar un numero? si/no.");
+
+	while(respuesta == "si")
+	{
+		contador ++;
+		numero = prompt("Ingrese un numero");
+		numero = parseInt(numero);
+
+			while(isNaN(numero)) //con esto nos aseguramos que no estamos ingresando un valor NaN
+			{
+				numero=prompt("Ingrese un numero");
+				numero=parseInt(numero);
+			}
+		
+			acumulador=numero+acumulador;
+			respuesta=prompt("Desea ingresar un numero? si/no.")
+
+	}
 
 
 document.getElementById('suma').value=acumulador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,12 +1,36 @@
 function Mostrar()
 {
-
+	var numero;
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
 	
 	var respuesta='si';
 
+	respuesta = prompt("Desea ingresar un numero? si/no.");
+
+	while(respuesta == "si")
+	{
+		numero = prompt("Ingrese un numero");
+		numero = parseInt(numero);
+			while (isNaN(numero)) 			//con esto nos aseguramos que no estamos ingresando un valo NaN
+				{
+					numero=prompt("Ingrese numero nuevamente porque no ingerso un numero correspondiente.");
+					numero=parseInt(numero);
+				}
+
+			if(numero > 0)
+			{
+				positivo=numero+positivo
+			}
+			else if(numero != 0)
+			{
+				negativo=numero*negativo
+			}
+
+		respuesta=prompt("Desea ingresar otro numero? si/no");
+
+	}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
