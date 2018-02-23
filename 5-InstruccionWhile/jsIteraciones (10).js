c79e2de4222b3f376d203f1d54ad396e899bd9d7
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-02-23 12:08:44 -0300

    ejercicio 10 incompleto
    while,isNaN, contadores if, else

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,76 @@
 function Mostrar()
 {
-
+	var numero;
 	var contador=0;
+	var negativos=0; //le doy un valor a las variables.
+	var acumuladorNegativos=0;
+	var positivos=0;
+	var acumuladorPositivos=0;
+	var contadorPares=0;
+	var contadorCeros=0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferencia;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
+	respuesta= prompt("Desea ingresar un numero?");
+
 	while(respuesta!="no")
-	{
-		
+	{	
+		contador ++;
+		numero=prompt("Ingrese un numero");
+		numero= parseInt(numero);
+			while(isNaN(numero))
+			{
+				numero=prompt("Ingrese solamente un numero.");
+				numero=parseInt(numero);
+			}
+		if(numero%2==0 && numero != 0)
+		{
+			contadorPares ++;
+		}
+
+		//suma de negativos
+		if(numero<0)
+		{
+			negativos=negativos+numero;
+			acumuladorNegativos ++;
+		}
+		//suma de positivos
+		else 
+		{
+			if(numero>0)
+			{
+			positivos=positivos+numero;
+			acumuladorPositivos ++;
+			}
+			else
+			{
+				contadorCeros ++;
+			}
+		}
+
+
+
+		respuesta= prompt("ingresar otro numero?");
+
+	}//cierra el while(respuesta!="no")
+	promedioNegativos=acumuladorNegativos / negativos;
+	promedioPositivos=acumuladorPositivos / positivos;
+
+	diferencia= acumuladorPositivos-acumuladorNegativos
+
+	document.write("La suma de los negativos es " + negativos);
 	
-	}
+	document.write("La suma de los positivos es " +positivos);
+
+	document.write("El promedio de los negativos es " +promedioNegativos)
+
+	document.write("El promedio de los positivos es " +promedioPositivos)
+
+	document.write("La diferencia es de "+diferencia)
 
 
 
