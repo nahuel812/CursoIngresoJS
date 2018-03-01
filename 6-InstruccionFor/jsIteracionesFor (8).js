4cb41c22e828735b8b9cb0a542ca8223fcd7fd06
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-03-01 11:33:27 -0300

    ejercicio 8 en clase
    for y if

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,5 +1,27 @@
 function Mostrar()
 {
+    var numero;
+    var contador=0;
+
+    numero=prompt("Ingrese un numero.");
+
+    for(var i=1;i<=numero;i++)
+    {
+        if(numero%i==0)
+        {
+            contador++;
+        }
+    }
+
+    if(contador==2)
+    {
+        alert(numero+"Es primo");
+    }
+    else
+    {
+        alert(numero+"No es primo");
+    }
+
 
 
 
