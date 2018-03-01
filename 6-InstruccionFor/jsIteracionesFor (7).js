54007ddacc0f2f04973d6c29b7a5fe0e88713878
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-03-01 11:27:47 -0300

    ejercicio 7 en clase
    for y if

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,5 +1,22 @@
 function Mostrar()
 {
+    var numero;
+    var cantidadDivisores=0;
+
+
+    numero=prompt("Ingrese un numero.")
+
+    for(i=1;i<=numero;i++)
+    {
+        
+        if(numero%i == 0)
+        {
+            cantidadDivisores++;
+            console.log("Numero divisor "+i);
+        }
+
+        console.log("Cantidad de divisores "+cantidadDivisores)
+    }
 
 
 
