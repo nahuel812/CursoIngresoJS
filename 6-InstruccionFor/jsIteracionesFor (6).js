9ea20bb77835dd6a9a3ec9f4778c2ddea907af02
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-03-01 10:17:46 -0300

    ejercicio 6 en clase
    for

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,5 +1,20 @@
 function Mostrar()
 {
+    var numero;
+    var contadorPares=0;
+
+
+    numero=prompt("Ingrese un numero.")
+
+    for(var i=1;i<numero;i++)
+    {
+        if(i%2 == 0 && numero!=0)
+        {
+            contadorPares++;
+            console.log(contadorPares);
+        }
+
+    }
 
 
 
